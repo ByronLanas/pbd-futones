795359ff38cbbdd50f6c29ec08d3618aa09caadd
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-04 23:36:06 +0000

    frames del menu y pagina inicial

--- futonline/View/Pages/vistaInicio.php
+++ futonline/View/Pages/vistaInicio.php
@@ -5,3 +5,22 @@
  * and open the template in the editor.
  */
 ?>
+<HTML>
+<HEAD><TITLE> Título de la página </TITLE></HEAD>
+
+     <FRAMESET COLS=20%,80% NORESIZE>
+
+           <FRAME SRC="vistaMenu.php" noresize="noresize">
+           <FRAME SRC="vistaPaginaInicio.php" noresize="noresize">
+
+     </FRAMESET>
+
+     <NOFRAMES>
+         <BODY>
+             Lo siento,su navegador no soporta frames.
+             Pulse <a href="frames1.htm>aquí </A> 
+             para acceder a los contenidos de estas páginas.
+         </BODY>
+     </NOFRAMES>
+
+</HTML>
