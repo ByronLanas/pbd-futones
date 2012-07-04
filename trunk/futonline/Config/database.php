19a63e3e1dff7b02f2e75668b4db54fedadfc290
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-04 19:07:39 +0000

    con cake y mvc

---
+++ trunk/futonline/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/sqlserver',
+		'persistent' => false,
+		'host' => '158.170.255.202',
+		'port' => 1433,
+		'login' => 'byron',
+		'password' => 'byron',
+		'database' => 'pbd',
+	);
+}
