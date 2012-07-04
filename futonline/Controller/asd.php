4a6cfefa5a2d0afa0050662d5b2863fb9a3b8366
diegog.asd@gmail.com <diegog.asd@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-04 23:32:13 +0000

    Aqui van algunos adelantos de lo que el Prof. Vermilion nos mando. Saludos.

---
+++ futonline/Controller/asd.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$usuario=$_REQUEST["usuario"];
+$password=$_REQUEST["password"];
+echo $usuario." y ".$password;
+?>
+
---
+++ futonline/View/Pages/pagina.php
@@ -0,0 +1,14 @@
+<?php
+
+$a=$_REQUEST["evalua"];
+if($a=="NICOLAS"){
+    echo "<table border=1><tr><td>asd1</td></tr>
+        <tr><td>asd2</td></tr>
+        <tr><td>asd3</td></tr>
+        <tr><td>asd4</td></tr>
+        <tr><td>asd5</td></tr></table>";
+}
+else{
+    echo "MUY MAL";
+}
+?>
