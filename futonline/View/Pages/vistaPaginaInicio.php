9bfd757a295e08291bf7e0a4211ab24eadc1d464
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 15:18:41 +0000

    pagina de inicio

--- futonline/View/Pages/vistaPaginaInicio.php
+++ futonline/View/Pages/vistaPaginaInicio.php
@@ -1,9 +1,12 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+?>
+<body style="background-color:#0A8A9A; font: Helvetica 12pt;">
+<img src="../Elements/logo.jpg" width="215" height="56" alt="logo" align="rigth" /><br><br><br><br>
+<div align="center" ><img src="../Elements/foto.jpg" width="846" height="397" alt="foto" align="center"/>
+=======
 ?>
 <img src="../Elements/logo.jpg" width="215" height="56" alt="logo" /><br><br><br>
 <img src="../Elements/foto.jpg" width="846" height="397" alt="foto"/>
+
