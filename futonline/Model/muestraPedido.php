a0efb3c66d86d2567f06e54faeea731a839088a6
diegog.asd@gmail.com <diegog.asd@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 20:52:37 +0000

    Aqui van algunos adelantos de lo que el Prof. Vermilion nos mando. Saludos.

--- futonline/Model/muestraPedido.php
+++ futonline/Model/muestraPedido.php
@@ -1,23 +1,26 @@
 <?php
-class muestraProducto{
+        
+class muestraPedido{
     
     var $link;
-    public function muestraProducto(){
+    public function muestraPedido(){
       //$dsn = "Driver={SQL Server Native Client 10.0};Server=NOTE\SQLEXPRESS;Database=PBD;Integrated Security=SSPI;Persist Security Info=False;";
       $dsn = "Driver={SQL Server Native Client 11.0};Server=NOTE-PC\DIEGO;Database=PBD;Integrated Security=SSPI;Persist Security Info=False;";
       $this->link= odbc_connect($dsn/*HOST*/, 'byron'/*USER*/, 'byron'/*PASSWORD*/) or die('Critical Error: No se ha podido conectar a la base de datos') ;                    
       
     }
-    public function tipoProducto($tipoProducto){
-                    
-        $sql = "select * from PRODUCTO where TIP_PROD= '$tipoProducto'";
+    public function consultar_pedido($usuario){        
+        $sql = "SELECT cli_nom FROM cliente WHERE cli_rut='$usuario'";
         
         $result= odbc_exec($this->link,$sql)or die(exit("Error en odbc_exec"));
-        
+        $nombre = odbc_result($result,"CLI_NOM");
+        $_SESSION["nombre"]=$nombre;
+        $sql = "SELECT * FROM pedido WHERE cli_rut='$usuario'";
+        $result= odbc_exec($this->link,$sql)or die(exit("Error en odbc_exec"));
         while($aux=odbc_fetch_array($result))
-            $productos[]=$aux;
-        return $productos;
-
-    }
+            $pedidos[]=$aux;
+        return $pedidos;
+        
+}   
 }
 ?>
---
+++ futonline/View/Pages/vistaPedido.php
@@ -0,0 +1,17 @@
+<?php
+//session_start();
+$nombre = $_SESSION["nombre"];
+$pedidos = $_SESSION["pedidos"]
+?>
+<body> Pedidos del usuario: <?php echo $nombre;?> <br></body>
+<table border="1">
+        <td>
+            <tr>Estado</tr>
+            <tr>Fecha</tr>
+        </td>
+</table>
+
+
+	        
+
+
