ab97e7c116bb957f57d46a37f2a3e2b7a49be702
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 19:51:28 +0000

    catalago para mas de un producto

--- futonline/View/Pages/vistaCatalogo.php
+++ futonline/View/Pages/vistaCatalogo.php
@@ -5,5 +5,27 @@ header( 'Content-Type: text/html;charset=utf-8' );
 <META http-equiv=Content-Type content="text/html; charset=utf-8">
 <body style="background-color:#0A8A9A; font: Helvetica 12pt;">
 
-    <a href="../../Controller/ControladorCatalogo.php?tipoProducto=futon"><img src="../Elements/futon.jpg" width="312" height="246" alt="futon"/>
-    </a>    
+  <style type="text/css">
+<!--
+.Estilo2 {font-size: 13px}
+.mm {
+	text-align: center;
+}
+-->
+</style>    
+    
+    <div >
+        <a href="../../Controller/ControladorCatalogo.php?tipoProducto=futon">
+            <img src="../Elements/futon.jpg" width="312" height="246" alt="futon"/><br>
+            <span class="Estilo2"><strong>FUTONES</strong></span>
+        </a>
+    </div>
+    <div >
+        <a href="../../Controller/ControladorCatalogo.php?tipoProducto=pera">
+            <img src="../Elements/peras.jpg" width="415" height="347" alt="peras"/><br>
+            <span class="Estilo2"><strong>FUTONES</strong>
+  <br></span></a></div>
+<div ><a href="../../Controller/ControladorCatalogo.php?tipoProducto=futon"><img src="../Elements/futon.jpg" width="312" height="246" alt="futon"/><br><span class="Estilo2"><strong>FUTONES</strong>
+  <br></span></a></div>
+<div ><a href="../../Controller/ControladorCatalogo.php?tipoProducto=futon"><img src="../Elements/futon.jpg" width="312" height="246" alt="futon"/><br><span class="Estilo2"><strong>FUTONES</strong>
+  <br></span></a></div>
