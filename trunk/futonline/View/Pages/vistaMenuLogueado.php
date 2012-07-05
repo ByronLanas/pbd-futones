4cce36594d0e0a61d0e1d3914009be73764731af
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 14:16:29 +0000

    pagina de inicio

--- trunk/futonline/View/Pages/vistaMenuLogueado.php
+++ trunk/futonline/View/Pages/vistaMenuLogueado.php
@@ -5,4 +5,4 @@ $usuario=$_SESSION["usuario"];
 ?>
 
 <a href="vistaDatosUsuario.php" target="fPagina"> <?php echo $usuario;?></a><br>
-<a href="/futonline/View/Pages/vistaInicio.php" target="_parent">Cerrar Sesion</a>
+<a href="/futonline/View/Pages/vistaInicio.php" target="_parent">Cerrar Sesion</a><br><br>
--- trunk/futonline/View/Pages/vistaPaginaInicio.php
+++ trunk/futonline/View/Pages/vistaPaginaInicio.php
@@ -4,6 +4,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-?>
-<img src="../Elements/logo.jpg" width="215" height="56" alt="logo"/><br>
+?><div align="rigth" color
+<img src="../Elements/logo.jpg" width="215" height="56" alt="logo" /><br>
 <img src="../Elements/foto.jpg" width="846" height="397" alt="foto"/>
