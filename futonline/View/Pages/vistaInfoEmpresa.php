cfe38c8fc6bd5b44172b50ea501dd7807d42c8d8
diegog.asd@gmail.com <diegog.asd@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 15:14:50 +0000

    Aqui van algunos adelantos de lo que el Prof. Vermilion nos mando. Saludos.

--- futonline/View/Pages/vistaInfoEmpresa.php
+++ futonline/View/Pages/vistaInfoEmpresa.php
@@ -1,3 +1,7 @@
+<?php 
+header( 'Content-Type: text/html;charset=utf-8' );  
+?>
+<META http-equiv=Content-Type content="text/html; charset=utf-8">
 <br>
 <div align="center">
   <style type="text/css">
