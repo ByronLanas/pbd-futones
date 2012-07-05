2d16c27ace02922cb8dc43fb411d17c60c2f529e
Battousai3@gmail.com <Battousai3@gmail.com@0cc06948-967c-1e33-a04f-2e9d9fb00926>
2012-07-05 16:06:12 +0000

    colores menu logueado y cerrar sesion mejorado

--- trunk/futonline/View/Pages/vistaPaginaInicio.php
+++ trunk/futonline/View/Pages/vistaPaginaInicio.php
@@ -5,8 +5,4 @@
 <body style="background-color:#0A8A9A; font: Helvetica 12pt;">
 <img src="../Elements/logo.jpg" width="215" height="56" alt="logo" align="rigth" /><br><br><br><br>
 <div align="center" ><img src="../Elements/foto.jpg" width="846" height="397" alt="foto" align="center"/>
-=======
-?>
-<img src="../Elements/logo.jpg" width="215" height="56" alt="logo" /><br><br><br>
-<img src="../Elements/foto.jpg" width="846" height="397" alt="foto"/>
 
